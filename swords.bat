25e90dccfa5ca08a296144f4cab6f6fa9f55f6ea
takasehideki <takase@i.kyoto-u.ac.jp>
2017-05-19 17:39:35 +0900

    generate output dir as projectname

--- swords.bat
+++ swords.bat
@@ -3,9 +3,12 @@
 SET toolchainpath=%~dp0
 CALL %toolchainpath%\setenv.bat
   
-SET cfile=%~1
-SET jsonfile=%~2
+SET cfile=..\%~1
+SET jsonfile=..\%~2
 SET projectname=%~3
+
+mkdir %projectname%
+cd %projectname%
 
 if not {%~1} == {} if not {%~2} == {} if not {%~3} == {} (
   if not {%llvmfilepath%} == {""} (
@@ -14,10 +17,14 @@ if not {%~1} == {} if not {%~2} == {} if not {%~3} == {} (
     %toolchainpath%\batch\ifmake.bat %cfile% %jsonfile% %toolchainpath%
   )
 
-  %toolchainpath%\batch\runhls.bat %cfile:~-0,-2%_hw_re.c %jsonfile% %projectname% %toolchainpath%
+  mv ..\%projectname%_* .
+
+  %toolchainpath%\batch\runhls.bat %cfile:~3,-2%_hw_re.c %jsonfile% %projectname% %toolchainpath%
   %toolchainpath%\batch\runvivado.bat %jsonfile% %projectname% %projectname%_hls %cd% %toolchainpath%
   %toolchainpath%\batch\runsdk.bat %cfile% %jsonfile% %projectname% %toolchainpath%
  
 ) else (
     echo "few arguments!"
 )
+
+cd ..
