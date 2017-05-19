5cc8eb5e02332bbdb785a731e91ac01bc1ee2efd
takasehideki <takase@i.kyoto-u.ac.jp>
2017-05-19 15:56:43 +0900

    modify environmental variables

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,4 @@
 @rem Xilinxツールのインストールディレクトリへのパス
-SET xilinxpath=C:\research\Xilinx
+SET xilinxpath=C:\Xilinx\SDSoC\2015.4
 @rem LLVMライブラリファイルへのパス
 @rem SET llvmfilepath="C:\Program Files (x86)\LLVM\bin\libclang.dll"
