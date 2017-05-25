b221a83bac4040aebe97bf98f4c1291f0e726e53
TAKASE hideki <takase@i.kyoto-u_ac_jp>
2017-05-26 08:50:53 +0900

    modify mv to move (cmd.exe command)

--- swords.bat
+++ swords.bat
@@ -17,7 +17,7 @@ if not {%~1} == {} if not {%~2} == {} if not {%~3} == {} (
     %toolchainpath%\batch\ifmake.bat %cfile% %jsonfile% %toolchainpath%
   )
 
-  mv ..\%projectname%_* .
+  move ..\%projectname%_* .
 
   %toolchainpath%\batch\runhls.bat %cfile:~3,-2%_hw_re.c %jsonfile% %projectname% %toolchainpath%
   %toolchainpath%\batch\runvivado.bat %jsonfile% %projectname% %projectname%_hls %cd%\%projectname% %toolchainpath%
