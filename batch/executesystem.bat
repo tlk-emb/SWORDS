cbd28855c3c103a21c74d160a57a93c1eeb072c9
takasehideki <takase@i.kyoto-u.ac.jp>
2017-05-31 19:43:41 +0900

    maintain config/execute batch

--- batch/executesystem.bat
+++ batch/executesystem.bat
@@ -1,18 +1,25 @@
 @echo off
 
+SET toolchainpath=%~dp0
+CALL %toolchainpath%\..\setenv.bat
+  
 SET sdkpath=%xilinxpath%\SDK\2015.4\bin
 
-SET cfile=%~1
-SET jsonfilepath=%~2
+SET cfile=..\%~1
+SET jsonfile=..\%~2
 SET projectname=%~3
 
 SET toolchainpath=%~dp0
 
 if not {%~1} == {} if not {%~2} == {} if not {%~3} == {} (
     
-  python %toolchainpath%\python\generateexecutesdktcl.py %cfile% %jsonfile% %projectname%
-  %sdkpath%\xsdk.bat -batch -source %projectname%_config_sdk.tcl
+  cd %projectname%
+
+  python %toolchainpath%\..\python\generateexecutesdktcl.py %cfile% %jsonfile% %projectname%
+  rem %sdkpath%\xsdk.bat -batch -source %projectname%_config_sdk.tcl
   %sdkpath%\xsdk.bat -batch -source %projectname%_execute_sdk.tcl
+
+  cd ..
 
 ) else (
   echo "few arguments!"
