8d4e0519ec246ce0b06dbb2aae6ddbede00a01b2
takasehideki <takase@i.kyoto-u.ac.jp>
2017-09-06 19:38:26 +0900

    Modify generatevivadotcl

--- python/generatevivadotcl.py
+++ python/generatevivadotcl.py
@@ -43,7 +43,10 @@ class generateVivadoTcl:
         self.s_axilite_bundles = []
         self.m_axi_bundles = []
         self.axis_bundles = []
+        # 不使用？？
         self.use_m_axi_ports = []
+        # Include segment用
+        self.use_m_axi_GP1 = False
 
         self.project_path = project_path.replace("\\","/")
         self.project_name = project_name
@@ -89,13 +92,15 @@ class generateVivadoTcl:
             elif (str(parameter["mode"])) == "axis":
                 if str(parameter["bundle"]) not in self.axis_bundles:
                     self.axis_bundles.append(str(parameter["bundle"]))
-        
+
+        '''
         for bundles_pair in self.m_axi_bundles: #使用するm_axiのポートを重複なく数える
             port = bundles_pair[1]
             if port == "ACP" or port == "HP0" or port == "HP1" or port == "HP2" or port == "HP3":
                 if port not in self.use_m_axi_ports:
                     self.use_m_axi_ports.append(port)
-        
+        '''
+
     def generateVivadoTcl(self):
 
         vivado_tcl = ""
@@ -124,31 +129,46 @@ class generateVivadoTcl:
         vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external \"FIXED_IO, DDR\" apply_board_preset \"1\" Master \"Disable\" Slave \"Disable\" }  [get_bd_cells processing_system7_0]\n"
         vivado_tcl += "endgroup\n"
 
+        # return用のポートを接続する
         vivado_tcl += "startgroup\n"
         vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master \"/processing_system7_0/M_AXI_GP0\" Clk \"Auto\" }  [get_bd_intf_pins %s_0/s_axi_AXILiteS]\n" % (self.func_name)
-        for s_axilite_bundle in self.s_axilite_bundles:
-            vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master \"/processing_system7_0/M_AXI_%s\" Clk \"Auto\" }  [get_bd_intf_pins %s_0/s_axi_%s]\n" % (s_axilite_bundle[1], self.func_name, s_axilite_bundle[0])
         vivado_tcl += "endgroup\n"
 
+        # HW処理の終了検知用の割込みピンを接続する
         vivado_tcl += "startgroup\n"
-
         vivado_tcl += "set_property -dict [list CONFIG.PCW_USE_FABRIC_INTERRUPT {1} CONFIG.PCW_IRQ_F2P_INTR {1}] [get_bd_cells processing_system7_0]\n"
+        vivado_tcl += "connect_bd_net [get_bd_pins %s_0/interrupt] [get_bd_pins processing_system7_0/IRQ_F2P]\n" % (self.func_name)
+        vivado_tcl += "endgroup\n"
+       
+        # s_axiliteを使用するbundleについて
+        if len(self.s_axilite_bundles) == 0:
+            pass
+        else: # s_axiliteがあるとき
+            for s_axilite_bundle in self.s_axilite_bundles:
+                vivado_tcl += "startgroup\n"
+                if s_axilite_bundle[1] == "GP1":
+                    vivado_tcl += "set_property -dict [list CONFIG.PCW_USE_M_AXI_%s {1}] [get_bd_cells processing_system7_0]\n" % (s_axilite_bundle[1])
+                    self.use_m_axi_GP1 = True
+                vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master \"/processing_system7_0/M_AXI_%s\" Clk \"Auto\" }  [get_bd_intf_pins %s_0/s_axi_%s]\n" % (s_axilite_bundle[1], self.func_name, s_axilite_bundle[0])
+                vivado_tcl += "endgroup\n"
+
+        # m_axiを使用するbundleについて
         if len(self.m_axi_bundles) == 0:
             pass
-        else: #m_axiがあるとき
-            for m_axi_port in self.use_m_axi_ports: 
-                vivado_tcl += "set_property -dict [list CONFIG.PCW_USE_S_AXI_%s {1}] [get_bd_cells processing_system7_0]\n" % (m_axi_port)
-        vivado_tcl += "endgroup\n"
+        else: # m_axiがあるとき
+            for m_axi_bundle in self.m_axi_bundles: 
+                vivado_tcl += "startgroup\n"
+                # 使用するポートを有効にしてbundleと接続する
+                vivado_tcl += "set_property -dict [list CONFIG.PCW_USE_S_AXI_%s {1}] [get_bd_cells processing_system7_0]\n" % (m_axi_bundle[1])
+                vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master \"/%s_0/m_axi_%s\" Clk \"Auto\" }  [get_bd_intf_pins processing_system7_0/S_AXI_%s]\n" % (self.func_name, m_axi_bundle[0], m_axi_bundle[1])
+                # address editorからInclude segmentする
+                vivado_tcl += "include_bd_addr_seg [get_bd_addr_segs -excluded %s_0/Data_m_axi_%s/SEG_processing_system7_0_%s_IOP]\n" % (self.func_name, m_axi_bundle[0], m_axi_bundle[1])
+                vivado_tcl += "include_bd_addr_seg [get_bd_addr_segs -excluded %s_0/Data_m_axi_%s/SEG_processing_system7_0_%s_M_AXI_GP0]\n" % (self.func_name, m_axi_bundle[0], m_axi_bundle[1])
+                # M_AXI_GP1を使用している時はそのbd_addr_segもInclude
+                if self.use_m_axi_GP1:
+                    vivado_tcl += "include_bd_addr_seg [get_bd_addr_segs -excluded %s_0/Data_m_axi_%s/SEG_processing_system7_0_%s_M_AXI_GP1]\n" % (self.func_name, m_axi_bundle[0], m_axi_bundle[1])
+                vivado_tcl += "endgroup\n"
 
-        for m_axi_bundle in self.m_axi_bundles:
-            vivado_tcl += "apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master \"/%s_0/m_axi_%s\" Clk \"Auto\" }  [get_bd_intf_pins processing_system7_0/S_AXI_%s]\n" % (self.func_name, m_axi_bundle[0], m_axi_bundle[1])
-
-        vivado_tcl += "connect_bd_net [get_bd_pins %s_0/interrupt] [get_bd_pins processing_system7_0/IRQ_F2P]\n" % (self.func_name)
-
-        for m_axi_bundle in self.m_axi_bundles: #ACPを使うならアドレスエディタをいじる必要がある
-            if m_axi_bundle[1] == "ACP":
-                vivado_tcl += "include_bd_addr_seg [get_bd_addr_segs -excluded %s_0/Data_m_axi_%s/SEG_processing_system7_0_ACP_IOP]\n" % (self.func_name, m_axi_bundle[0])
-                vivado_tcl += "include_bd_addr_seg [get_bd_addr_segs -excluded %s_0/Data_m_axi_%s/SEG_processing_system7_0_ACP_M_AXI_GP0]\n" % (self.func_name, m_axi_bundle[0])
 
         vivado_tcl += "make_wrapper -files [get_files %s/%s_vivado/%s.srcs/sources_1/bd/%s_system/%s_system.bd] -top\n" % (self.project_path, self.project_name, self.project_name, self.func_name, self.func_name)
 
