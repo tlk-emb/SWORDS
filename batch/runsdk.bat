2d9de276aa2089183e03b1f169f0b23b15928e1a
takasehideki <takase@i.kyoto-u.ac.jp>
2017-12-21 17:13:40 +0900

    Delete copy statements of timer.[ch]

--- batch/runsdk.bat
+++ batch/runsdk.bat
@@ -17,8 +17,6 @@ if not {%~1} == {} if not {%~2} == {} if not {%~3} == {} (
   copy %ciffile% software
   copy %cswfile% software\helloworld.c
   copy %toolchainpath%\utils\lscript.ld software
-  rem copy %toolchainpath%\utils\timer.c software
-  rem copy %toolchainpath%\utils\timer.h software
   python %toolchainpath%\python\generatesdktcl.py %cfile% %jsonfile% %projectname% %toolchainpath%
   %sdkpath%\xsdk.bat -batch -source %projectname%_build_sdk.tcl
 
