d54a5518c7255c5353df9c47d3e98ffb46c2c11c
nittax <lp6m2014@gmail.com>
2018-01-12 11:46:26 +0900

    resuport old format json file (designer can specify interface explicitly)

--- python/analyzer/jsonparam.py
+++ python/analyzer/jsonparam.py
@@ -25,7 +25,7 @@ SCHEMA = {
                 "offset": {"type": "string"},
                 "bundle": {"type": "string"},
                 "direction": {"type": "string"},
-                "num": {"type": "string"}
+                "num": {"type": "integer"}
             },
             "required": ["name", "num"]
         },
@@ -41,10 +41,17 @@ SCHEMA = {
             "properties": {
                 "name": {"type": "string"},
                 "return_type": {"type": "string"},
+                "mode": {"type": "string"},
                 "arguments": {
                     "type": "array",
                     "items": {"$ref": "#/definitions/hardware_task_argument"}
-                }
+                },
+                "bundles": {
+                    "type": "array",
+                    "items": {"$ref": "#/definitions/hardware_task_bundle"}
+                },
+                "use_stream": {"type": "boolean"},
+                "auto_interface_select" : {"type": "boolean"}
             },
             "required": ["name", "return_type", "arguments"]
         },
@@ -86,12 +93,14 @@ class SoftwareTask(Task):
         return SoftwareTask(node["name"])
 
 class HardwareTask(Task):
-    def __init__(self, name, return_type, arguments):
+    def __init__(self, mode, name, auto_interface_select, return_type, arguments, use_stream):
         super(HardwareTask, self).__init__(name)
-        self.mode = ""
+        self.mode = mode
         self.return_type = return_type
         self.arguments = arguments
         self.bundles = []
+        self.use_stream = use_stream
+        self.auto_interface_select = auto_interface_select
 
     @staticmethod
     def parse_config(node):
@@ -99,10 +108,24 @@ class HardwareTask(Task):
         return_type = node["return_type"]
         arguments = [HardwareTaskArgument.parse_config(n)
                      for n in node["arguments"]]
+        #ストリームはデフォルトでオフ
+        use_stream = False
+        if node.get("use_stream") is not None: use_stream = node["use_stream"]
+        #バンドル指定ならバンドルよみこみ
+        bundles = []
+        if node.get("bundles") is not None:
+            bundles = [HardwareTaskBundle.parse_config(n)
+                        for n in node["bundles"]]
+        #mode指定ならmodeよみこみ
+        mode = None
+        if node.get("mode") is not None:
+            mode = node["mode"]
+        #自動インタフェース選択はデフォルトでオン
+        auto_interface_select = True
+        if node.get("auto_interface_select") is not None:
+            auto_interface_select = node["auto_interface_select"]
 
-        return HardwareTask(name, return_type, arguments)
-
-
+        return HardwareTask(mode, name, auto_interface_select, return_type, arguments, use_stream)
 class HardwareTaskArgument(object):
     def __init__(self, name, arg_type, offset=None, direction=None, num=None):
         self.name = name
@@ -199,15 +222,125 @@ class TasksConfig(object):
     #通信プロトコル・ポートの選択を行う
     @staticmethod
     def select_connection(config):
-        hwtask_name = "matrixmul"
-        config.hardware_tasks[hwtask_name].mode = "s_axilite"
-        config.hardware_tasks[hwtask_name].arguments[0].mode = "m_axi"
-        config.hardware_tasks[hwtask_name].arguments[1].mode = "s_axilite"
-        config.hardware_tasks[hwtask_name].arguments[2].mode = "m_axi"
-        config.hardware_tasks[hwtask_name].arguments[0].bundle = "bundle_a"
-        config.hardware_tasks[hwtask_name].arguments[1].bundle = "bundle_b"
-        config.hardware_tasks[hwtask_name].arguments[2].bundle = "bundle_a"
-        config.hardware_tasks[hwtask_name].bundles = [HardwareTaskBundle("bundle_a", "ACP"), HardwareTaskBundle("bundle_b", "GP0")]
+        intBytesize = 32/8
+        #各HWタスクについて
+        for hwtask_name in config.hardware_tasks.keys():
+            ####################################################
+            ##### インタフェースが手動指定の場合はスキップ #####
+            ####################################################
+            if config.hardware_tasks[hwtask_name].auto_interface_select == False: continue
+
+            if config.hardware_tasks[hwtask_name].return_type is not "void":
+                config.hardware_tasks[hwtask_name].mode = "s_axilite"
+            # config.hardware_tasks[hwtask_name].arguments[0].mode = "m_axi"
+            # config.hardware_tasks[hwtask_name].arguments[1].mode = "s_axilite"
+            # config.hardware_tasks[hwtask_name].arguments[2].mode = "m_axi"
+            # config.hardware_tasks[hwtask_name].arguments[0].bundle = "bundle_a"
+            # config.hardware_tasks[hwtask_name].arguments[1].bundle = "bundle_b"
+            # config.hardware_tasks[hwtask_name].arguments[2].bundle = "bundle_a"
+            # config.hardware_tasks[hwtask_name].bundles = [HardwareTaskBundle("bundle_a", "ACP"), HardwareTaskBundle("bundle_b", "GP0")]
+
+            #通信プロトコルを決定する
+            for i,argument in enumerate(config.hardware_tasks[hwtask_name].arguments):
+                if argument.num * intBytesize < 400:
+                    config.hardware_tasks[hwtask_name].arguments[i].mode = "s_axilite"
+                else:
+                    config.hardware_tasks[hwtask_name].arguments[i].mode = "m_axi"
+                    config.hardware_tasks[hwtask_name].arguments[i].offset = "slave"
+
+            #定まった通信プロトコルについてbundleでまとめる
+            #通信プロトコルの集合を求める
+            protocol_dic = {} #key: m_axi, s_axilite, axis value: bundle_name
+            bundle_dic = {} #key: bundle_name value: data_byte_size
+            for i,argument in enumerate(config.hardware_tasks[hwtask_name].arguments):
+                bundle_name = ""
+                if argument.mode not in protocol_dic:
+                    bundle_name = "bundle_" + chr(ord('a') + len(bundle_dic))
+                    bundle_dic[bundle_name] = argument.num * intBytesize
+                    protocol_dic[argument.mode] = bundle_name
+                else:
+                    bundle_name = protocol_dic[argument.mode]
+                    bundle_dic[bundle_name] += argument.num * intBytesize
+                config.hardware_tasks[hwtask_name].arguments[i].bundle = bundle_name
+                print(i)
+                print(bundle_name)
+
+            #各バンドルの通信ポートをデータサイズが大きい順にソートする
+            bundle_dic_sorted = sorted(bundle_dic.items(), key=lambda x: -x[1])
+            #各ポートの使用可能最大数
+            max_GP = 2
+            max_ACP = 1
+            max_HP = 4
+            #[bundle_name, data_byte_size]のリスト
+            GP_port_list = []
+            ACP_port_list = []
+            HP_port_list = []
+            #各バンドルの通信データ量から使用する通信ポートを仮に決定する,各ポートの使用可能最大数は無視
+            for bundle_name in bundle_dic.keys():
+                data_byte_size = bundle_dic[bundle_name]
+                print(data_byte_size)
+                port_name = ""
+                if data_byte_size < 64:
+                    port_name = "GP"
+                    GP_port_list.append([bundle_name, data_byte_size])
+                elif data_byte_size < 64 * 1000:
+                    port_name = "ACP"
+                    ACP_port_list.append([bundle_name, data_byte_size])
+                else:
+                    port_name = "HP"
+                    HP_port_list.append([bundle_name, data_byte_size])
+            #各通信ポートについて使用可能最大数を超えているものについてデータサイズが一番小さなものを別の通信ポートに変更する
+            while True:
+                #ACPポート数超過
+                if len(ACP_port_list) > max_ACP:
+                    change_port = min(ACP_port_list, key=lambda x: x[1])
+                    change_port_bundle_name = change_port[0]
+                    change_port_data_byte_size = change_port[1]
+                    if change_port_data_byte_size < 64 and len(GP_port_list) + 1 < max_GP:
+                        new_port = "GP" + str(len(GP_port_list))
+                        ACP_port_list.remove(change_port)
+                        GP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                    elif len(HP_port_list) + 1 < max_HP:
+                        new_port = "HP" + str(len(HP_port_list))
+                        ACP_port_list.remove(change_port)
+                        HP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                #GPポート超過
+                if len(GP_port_list) > max_GP:
+                    change_port = min(GP_port_list, key=lambda x: x[1])
+                    change_port_bundle_name = change_port[0]
+                    change_port_data_byte_size = change_port[1]
+                    if change_port_data_byte_size < 64 * 10000 and len(ACP_port_list) + 1 < max_ACP:
+                        new_port = "ACP"
+                        GP_port_list.remove(change_port)
+                        ACP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                    elif len(HP_port_list) + 1 < max_HP:
+                        new_port = "HP" + str(len(HP_port_list))
+                        GP_port_list.remove(change_port)
+                        HP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                #HPポート超過
+                if len(HP_port_list) > max_HP:
+                    change_port = min(HP_port_list, key=lambda x: x[1])
+                    change_port_bundle_name = change_port[0]
+                    change_port_data_byte_size = change_port[1]
+                    #HPポートが選ばれているということは大容量なのでACP優先
+                    if len(ACP_port_list) + 1 < max_ACP:
+                        new_port = "ACP"
+                        HP_port_list.remove(change_port)
+                        ACP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                    elif len(GP_port_list) + 1 < max_GP:
+                        new_port = "GP" + str(len(GP_port_list))
+                        HP_port_list.remove(change_port)
+                        GP_port_list.append([change_port_bundle_name, change_port_data_byte_size])
+                
+                #すべての通信ポートが使用可能最大数以内におさまっていれば終了
+                if (len(GP_port_list) <= max_GP and len(ACP_port_list) <= max_ACP and len(HP_port_list) <= max_HP): break
+                
+            #決定した通信ポートをconfigに設定する
+            config.hardware_tasks[hwtask_name].bundles.append(HardwareTaskBundle(ACP_port_list[0][0], "ACP"))
+            for i, GP_port in enumerate(GP_port_list):
+                config.hardware_tasks[hwtask_name].bundles.append(HardwareTaskBundle(GP_port[0], "GP" + str(i)))
+            for i, HP_port in enumerate(HP_port_list):
+                config.hardware_tasks[hwtask_name].bundles.append(HardwareTaskBundle(HP_port[0], "HP" + str(i)))
 
         return config
 
