288b8c85fcadd557c4c80612477b2dad4f888c40
lp6m <lp6m2014@gmail.com>
2018-01-19 20:30:18 +0900

    insert sleep command after board reconnection for stability

--- batch/executesystem.bat
+++ batch/executesystem.bat
@@ -16,6 +16,7 @@ if not {%~1} == {} (
   cd %projectname%
   %sdkpath%\xmd.bat -tcl %arm_reset_tcl:\=/%
   %sdkpath%\xmd.bat -tcl %arm_reconnect_tcl:\=/%
+  timeout 5
   %sdkpath%\xsdk.bat -batch -source %projectname%_config_sdk.tcl
   %sdkpath%\xsdk.bat -batch -source %projectname%_execute_sdk.tcl
 
